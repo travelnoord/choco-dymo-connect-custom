ba946bb5c17be4d9e2d65b5c5e9908c88fe1aaa0
Peter Dorn <peter@made.frl>
2024-11-19 15:54:54 +0100

    Configure choco install

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'dymo-connect-custom*'
+  fileType       = 'exe'
+  silentArgs     = "/S /V`"/qn /norestart`""
+  validExitCodes = @(0,1641,3010)
+  url            = '\\nas.internoord.local\software\dymo-connect\DCDSetup1.4.6.37.exe'
+  checksum       = '82272f159dbc2318989681056db5a0a3e502a4d780807a50ecd849f30845e644'
+  checksumType   = 'sha256'
+  destination    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+
+Install-ChocolateyPackage @packageArgs
